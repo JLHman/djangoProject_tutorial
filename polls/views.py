2d8080cf48fddecd31ad86690fc941b07bf741fd
Jesse Heckman <jlheckman@gmail.com>
2022-10-31 19:00:41 -0500

    additions

--- polls/views.py
+++ polls/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
-# ...
+from django.utils import timezone
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -39,6 +41,9 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
 
 '''def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
